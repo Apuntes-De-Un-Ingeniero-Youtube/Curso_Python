5361d1d49cde412d61fd64196c7502143e13e832
Apuntes De Un Ingeniero <juank2001estevezvargas@gmail.com>
2022-11-08 10:07:18 -0500

    actualizacion de favoritos json

---
+++ 11. JSON/favoritos/crud/actualizar_favorito.py
@@ -0,0 +1,47 @@
+import json
+
+
+def update_favorito():
+    lista = []
+
+    # Solicitando el favorito a modificar
+    titulo_modificar = input("Ingrese el título a modificar ")
+
+    archivo = open("11. JSON/favoritos/favoritos.json", "r")
+    datos = json.load(archivo)
+    archivo.close()
+
+    # Eliminando el favorito
+    
+        del datos[titulo_modificar]
+    else:
+        print("No existe el favorito en el archivo")
+
+    # Escribiendo en el archivo.json
+    archivo = open("11. JSON/favoritos/favoritos.json", "w")
+    json.dump(datos, archivo)
+    archivo.close()
+
+    # Solicitando los datos a modificar
+    nuevo_titulo = input("Ingresa el n8evo tpitulo ")
+    nueva_url = input("Ingresa la nueva URL ")
+    nuevo_comentario = input("Ingresa el nuevo comentario ")
+
+    # Agregando el nuevo comentario y la nueva URL a la lista
+    lista.append(nueva_url)
+    lista.append(nuevo_comentario)
+
+    archivo = open("11. JSON/favoritos/favoritos.json", "r")
+    datos = json.load(archivo)
+    archivo.close()
+
+    # Agregando el favorito al diccionario
+    datos[nuevo_titulo] = lista
+
+    # Escribiendo en el archivo.json
+    archivo = open("11. JSON/favoritos/favoritos.json", "w")
+    json.dump(datos, archivo)
+    archivo.close()
+
+
+update_favorito()
--- 11. JSON/favoritos/crud/agregar_favorito.py
+++ 11. JSON/favoritos/crud/agregar_favorito.py
@@ -1,5 +1,4 @@
 import json
-import os
 
 
 def add_favorito():
@@ -15,28 +14,14 @@ def add_favorito():
     lista.append(url)
     lista.append(comentario)
 
-    # Abriendo o creando el archivo.json
-    filename = r"11. JSON/favoritos/favoritos.json"
-    if os.path.isfile(filename):
-        archivo = open("11. JSON/favoritos/favoritos.json", "r")
-        datos = json.load(archivo)
-        archivo.close()
+    archivo = open("11. JSON/favoritos/favoritos.json", "w")
 
-        datos[titulo] = lista
+    # Agregando el favorito al diccionario
+    diccionario[titulo] = lista
 
-        # Escribiendo los datos en el archivo
-        archivo = open("11. JSON/favoritos/favoritos.json", "w")
-        json.dump(datos, archivo)
-        archivo.close()
-    else:
-        archivo = open("11. JSON/favoritos/favoritos.json", "w")
-
-        # Agregando el favorito al diccionario
-        diccionario[titulo] = lista
-
-        # Escribiendo el favorito en el archivo .json
-        json.dump(diccionario, archivo)
-        archivo.close()
+    # Escribiendo el favorito en el archivo .json
+    json.dump(diccionario, archivo)
+    archivo.close()
 
 
 add_favorito()
