c6050caa6b71f7a49a8195a0294747269fa14314
Juan Carlos Estevez Vargas <juank2001estevezvargas@gmail.com>
2022-12-26 13:01:21 -0500

    insercion de varios registros

---
+++ 14. bases de datos/mysql/05. insercion_de_varios_registros.py
@@ -0,0 +1,51 @@
+import mysql.connector
+
+conexion = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    db="prueba_mysql")
+
+# Crear un cursosr para ejecutar consultas
+cursor = conexion.cursor()
+
+# Definir el SQL para insertar datos en la tabla
+# insertar_tabla_paciente_sql = """
+#     INSERT INTO medico (
+#         nombre,
+#         tipo_identificacion,
+#         numero_tarjeta_profesional,
+#         anios_experiencia,
+#         especialidad,
+#         hora_inicio_atencion,
+#         hora_fin_atencion
+#     ) VALUES (%s,%s,%s,%s,%s,%s,%s);
+# """
+# valores = ("Juan Médico", "CC", "#F78", 5.7, "Urología", 8.30, 16.25)
+
+# Definir el SQL para insertar datos en la tabla
+# insertar_tabla_paciente_sql = """
+#     INSERT INTO medico (
+#         nombre,
+#         especialidad,
+#         hora_fin_atencion
+#     ) VALUES (%s,%s,%s);
+# """
+# valores = ("Juan Médico 2", "Neurología", 18)
+
+# Definir el SQL para insertar todos datos en la tabla
+insertar_tabla_paciente_sql = """
+    INSERT INTO medico VALUES (%s,%s,%s,%s,%s,%s,%s,%s);
+"""
+valores = ((899, "Juan Médico", "CC", "#F7I898", 5.7, "Urología", 8.30, 16.25),
+           (345, "Juan Médico 2", "CC 2", "#F7I8 i2", 5.7, "Urología2", 8.30, 16.25))
+
+# Ejecutar consulta
+cursor.executemany(insertar_tabla_paciente_sql, valores)
+
+# Haciendo el commit
+conexion.commit()
+
+# Cerrando objetos
+cursor.close()
+conexion.close()
--- 14. bases de datos/sqlite/05. insercion_varios_registros.py
+++ 14. bases de datos/sqlite/05. insercion_varios_registros.py
@@ -15,11 +15,18 @@ insertar_tabla_paciente_sql = """
         VALUES (?, ?, ?, ?, ?, ?);
 """
 
-valores = (1, "Juan Estevez", "2001/05/01", "CC", "EPS 1", "Sufre de algo")
+valores = [
+    (11, "Juan Estevez", "2001/05/01", "CC", "EPS 1", "Sufre de algo"),
+    (2, "Pedro Alzate", "2010/05/11", "TI", "EPS 2", "Sufre de artritis"),
+    (3, "Juan Perez", "1998/05/28", "CC", "EPS 1", "No sufre de nada"),
+    (4, "Maria Becerra", "1966/11/01", "CC", "EPS 1", "Sufre de todo"),
+]
 
-cursor.execute(insertar_tabla_paciente_sql, valores)
+cursor.executemany(insertar_tabla_paciente_sql, valores)
 
 conexion.commit()
+
+print(f"Se han insertado {cursor.rowcount} registros")
 
 cursor.close()
 conexion.close()
